3949a9f4073ffc02b6576f478020d35de9a81404
Nitin Kanzariya <202201181@daiict.ac.in>
2024-11-18 16:36:58 +0530

    fix error

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,27 +1,36 @@
-import { useEffect } from "react";
-import Cookies from "js-cookie"; // Import js-cookie
+import { useEffect, useState } from "react";
 import Banner from "./Movies/Banner";
 import Footer from "./Movies/Footer";
 import MoviesContainerPage from "./Movies/MoviesContainerPage";
 import { useNavigate } from "react-router-dom";
+import Login from "./Auth/Login";
 
 const Home = () => {
   const navigate = useNavigate();
 
-  useEffect(() => {    
-  const userToken = Cookies.get("jwt");
-  if (!userToken) {
-    navigate("/login");
-  }
-  }, [navigate]);
+  const [userinfo, setUserinfo] = useState(false);
 
+  useEffect(() => {
+    if (window.localStorage.getItem("userInfo")) {
+      setUserinfo(true);
+    } else {
+      setUserinfo(false);
+      navigate("/login");
+    }
+  }, [navigate]);
 
   return (
     <>
       <section className="mt-[3px]">
-        <Banner />
-        <MoviesContainerPage />
-        <Footer />
+        {userinfo ? (
+          <>
+            <Banner />
+            <MoviesContainerPage />
+            <Footer />
+          </>
+        ) : (
+          <Login />
+        )}
       </section>
     </>
   );
